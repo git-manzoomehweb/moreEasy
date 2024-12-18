3b1d369b34b4983f150fc6da65dafb6d4d971a74
kmg.d102 <kmg.d102@gmail.com>
2024-12-18 05:56:39 -0800

    mobile page started

---
+++ design/src/js/easyAndMore.js
@@ -0,0 +1,23 @@
+const headerMenu = document.querySelector(".header-menu");
+const headerMenuClose = document.querySelector(".header-menu-close");
+const bars3 = document.querySelector(".bars3");
+
+if (innerWidth >= 1024) {
+  headerMenuClose.addEventListener("click", function () {
+    headerMenu.style.visibility = "hidden";
+    headerMenu.style.opacity = "0";
+    // headerMenu.style.display = "none";
+  });
+  bars3.addEventListener("click", function () {
+    headerMenu.style.visibility = "visible";
+    headerMenu.style.opacity = "1";
+    // headerMenu.style.display = "block";
+  });
+} else {
+  headerMenuClose.addEventListener("click", function () {
+    headerMenu.style.transform = "translateX(1024px)";
+  });
+  bars3.addEventListener("click", function () {
+    headerMenu.style.transform = "translateX(0)";
+  });
+}
