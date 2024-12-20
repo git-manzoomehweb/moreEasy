85e5683d420c09f0127ade0e05e9b2e8e0818c62
SinaSfr <anonymous8884person@gmail.com>
2024-12-21 00:04:51 +0330

    pc page started

--- design/src/js/easyAndMore.js
+++ design/src/js/easyAndMore.js
@@ -22,81 +22,105 @@ if (window.innerWidth >= 1024) {
   });
 }
 
+document.addEventListener("DOMContentLoaded", function () {
+  const emailIcon = document.querySelector(".email-icon");
+  const emailPopup = document.querySelector(".email-popup");
+  const closeIcon = document.querySelector(".close-icon");
 
-document.addEventListener('DOMContentLoaded', function () {
-  const emailIcon = document.querySelector('.email-icon');
-  const emailPopup = document.querySelector('.email-popup');
-  const closeIcon = document.querySelector('.close-icon');
-
-  emailIcon.addEventListener('click', function () {
-      if (window.innerWidth < 1024) {
-          emailPopup.classList.toggle('opacity-0'); // Toggle opacity for mobile
-          emailPopup.classList.toggle('translate-x-full'); // Toggle translate for mobile
-          emailPopup.classList.toggle('pointer-events-none'); // Allow pointer events when visible
-      } else {
-          emailPopup.classList.remove('opacity-0'); // Show popup for desktop
-          emailPopup.classList.remove('translate-x-full'); // Reset translate for desktop
-          emailPopup.classList.remove('pointer-events-none'); // Allow pointer events
-          emailPopup.classList.add('translate-y-0'); // Move to center for desktop
-      }
+  emailIcon.addEventListener("click", function () {
+    if (window.innerWidth < 1024) {
+      emailPopup.classList.toggle("opacity-0"); // Toggle opacity for mobile
+      emailPopup.classList.toggle("translate-x-full"); // Toggle translate for mobile
+      emailPopup.classList.toggle("pointer-events-none"); // Allow pointer events when visible
+    } else {
+      emailPopup.classList.remove("opacity-0"); // Show popup for desktop
+      emailPopup.classList.remove("translate-x-full"); // Reset translate for desktop
+      emailPopup.classList.remove("pointer-events-none"); // Allow pointer events
+      emailPopup.classList.add("translate-y-0"); // Move to center for desktop
+    }
   });
 
-  closeIcon.addEventListener('click', function () {
-      if (window.innerWidth < 1024) {
-          emailPopup.classList.add('opacity-0'); // Hide popup
-          emailPopup.classList.add('translate-x-full'); // Add translate for hiding
-          emailPopup.classList.add('pointer-events-none'); // Disable pointer events
-      } else {
-          emailPopup.classList.add('opacity-0'); // Hide popup
-          emailPopup.classList.add('translate-y-[-50%]'); // Move up for hiding
-          emailPopup.classList.add('pointer-events-none'); // Disable pointer events
-      }
+  closeIcon.addEventListener("click", function () {
+    if (window.innerWidth < 1024) {
+      emailPopup.classList.add("opacity-0"); // Hide popup
+      emailPopup.classList.add("translate-x-full"); // Add translate for hiding
+      emailPopup.classList.add("pointer-events-none"); // Disable pointer events
+    } else {
+      emailPopup.classList.add("opacity-0"); // Hide popup
+      emailPopup.classList.add("translate-y-[-50%]"); // Move up for hiding
+      emailPopup.classList.add("pointer-events-none"); // Disable pointer events
+    }
   });
 });
 
-
-
-
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function () {
   const toggleButtons = document.querySelectorAll(".toggleButton");
   const tourLists = document.querySelectorAll(".tourList");
 
   toggleButtons.forEach((button, index) => {
-      button.addEventListener('click', function() {
-          const tourList = tourLists[index];
+    button.addEventListener("click", function () {
+      const tourList = tourLists[index];
 
-          button.classList.toggle("rotate-180")
-          tourList.classList.toggle("mt-6")
+      button.classList.toggle("rotate-180");
+      tourList.classList.toggle("mt-6");
 
-          if (tourList.classList.contains('max-h-0')) {
-              tourList.classList.remove('max-h-0', 'opacity-0');
-              tourList.classList.add('max-h-screen', 'opacity-100'); 
-          } else {
-              tourList.classList.add('max-h-0', 'opacity-0');
-              tourList.classList.remove('max-h-screen', 'opacity-100');
-          }
-      });
+      if (tourList.classList.contains("max-h-0")) {
+        tourList.classList.remove("max-h-0", "opacity-0");
+        tourList.classList.add("max-h-screen", "opacity-100");
+      } else {
+        tourList.classList.add("max-h-0", "opacity-0");
+        tourList.classList.remove("max-h-screen", "opacity-100");
+      }
+    });
   });
 });
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function () {
   const faqBtn = document.querySelectorAll(".faq-btn");
   const faqAnswers = document.querySelectorAll(".faq-answer");
 
   faqBtn.forEach((button, index) => {
-      button.addEventListener('click', function() {
-          const faqAnswer = faqAnswers[index];
+    button.addEventListener("click", function () {
+      const faqAnswer = faqAnswers[index];
 
-          button.classList.toggle("rotate-180")
-          faqAnswer.classList.toggle("mt-2")
+      button.classList.toggle("rotate-180");
+      faqAnswer.classList.toggle("mt-2");
 
-          if (faqAnswer.classList.contains('max-h-0')) {
-            faqAnswer.classList.remove('max-h-0', 'opacity-0');
-            faqAnswer.classList.add('max-h-screen', 'opacity-100'); 
-          } else {
-            faqAnswer.classList.add('max-h-0', 'opacity-0');
-            faqAnswer.classList.remove('max-h-screen', 'opacity-100');
-          }
-      });
+      if (faqAnswer.classList.contains("max-h-0")) {
+        faqAnswer.classList.remove("max-h-0", "opacity-0");
+        faqAnswer.classList.add("max-h-screen", "opacity-100");
+      } else {
+        faqAnswer.classList.add("max-h-0", "opacity-0");
+        faqAnswer.classList.remove("max-h-screen", "opacity-100");
+      }
+    });
   });
 });
+
+// position fixed
+const media = window.matchMedia("screen and (min-width:1540px)");
+if (media.matches) {
+ 
+   const floatingIconEl = document.querySelector(".floating-icons");
+   floatingIconEl.style.position = "fixed"; 
+   floatingIconEl.style.bottom = "0"; 
+   floatingIconEl.style.top = "unset"; 
+}else {
+   const footerEl = document.querySelector("footer"); 
+   const footerElOffsetTop = footerEl.offsetTop; 
+   const floatingIconEl = document.querySelector(".floating-icons"); 
+    window.addEventListener("scroll", function () {
+       const st = this.scrollY; 
+        
+       if (st + window.innerHeight > footerElOffsetTop) {
+           floatingIconEl.style.position = "absolute"; 
+           floatingIconEl.style.top = `${footerElOffsetTop - floatingIconEl.offsetHeight}px`;
+           floatingIconEl.style.bottom = "unset"; 
+       } else {
+           floatingIconEl.style.position = "fixed"; 
+           floatingIconEl.style.bottom = `0`; 
+           floatingIconEl.style.top = "unset"; 
+       }
+   });
+  }
+
