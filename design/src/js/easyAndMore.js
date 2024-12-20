78d3e8d7c0fbeb34ab049b5f26937171fdd9a82c
SinaSfr <anonymous8884person@gmail.com>
2024-12-20 14:43:13 +0330

    update mobile

--- design/src/js/easyAndMore.js
+++ design/src/js/easyAndMore.js
@@ -2,7 +2,7 @@ const headerMenu = document.querySelector(".header-menu");
 const headerMenuClose = document.querySelector(".header-menu-close");
 const bars3 = document.querySelector(".bars3");
 
-if (innerWidth >= 1024) {
+if (window.innerWidth >= 1024) {
   headerMenuClose.addEventListener("click", function () {
     headerMenu.style.visibility = "hidden";
     headerMenu.style.opacity = "0";
@@ -21,6 +21,40 @@ if (innerWidth >= 1024) {
     headerMenu.style.transform = "translateX(0)";
   });
 }
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  const emailIcon = document.querySelector('.email-icon');
+  const emailPopup = document.querySelector('.email-popup');
+  const closeIcon = document.querySelector('.close-icon');
+
+  emailIcon.addEventListener('click', function () {
+      if (window.innerWidth < 1024) {
+          emailPopup.classList.toggle('opacity-0'); // Toggle opacity for mobile
+          emailPopup.classList.toggle('translate-x-full'); // Toggle translate for mobile
+          emailPopup.classList.toggle('pointer-events-none'); // Allow pointer events when visible
+      } else {
+          emailPopup.classList.remove('opacity-0'); // Show popup for desktop
+          emailPopup.classList.remove('translate-x-full'); // Reset translate for desktop
+          emailPopup.classList.remove('pointer-events-none'); // Allow pointer events
+          emailPopup.classList.add('translate-y-0'); // Move to center for desktop
+      }
+  });
+
+  closeIcon.addEventListener('click', function () {
+      if (window.innerWidth < 1024) {
+          emailPopup.classList.add('opacity-0'); // Hide popup
+          emailPopup.classList.add('translate-x-full'); // Add translate for hiding
+          emailPopup.classList.add('pointer-events-none'); // Disable pointer events
+      } else {
+          emailPopup.classList.add('opacity-0'); // Hide popup
+          emailPopup.classList.add('translate-y-[-50%]'); // Move up for hiding
+          emailPopup.classList.add('pointer-events-none'); // Disable pointer events
+      }
+  });
+});
+
+
 
 
 document.addEventListener('DOMContentLoaded', function() {
