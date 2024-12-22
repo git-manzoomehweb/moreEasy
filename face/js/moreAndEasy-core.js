31d9e4833275dc9ad9eda2ded36e90ed9ba4bdf2
kmg.d102 <kmg.d102@gmail.com>
2024-12-22 06:04:29 -0800

    search engine updated

--- face/js/moreAndEasy-core.js
+++ face/js/moreAndEasy-core.js
@@ -29,27 +29,27 @@ document.addEventListener("DOMContentLoaded", function () {
 
   emailIcon.addEventListener("click", function () {
     if (window.innerWidth < 1024) {
-      emailPopup.classList.toggle("opacity-0"); 
-      emailPopup.classList.toggle("translate-x-full"); 
+      emailPopup.classList.toggle("opacity-0");
+      emailPopup.classList.toggle("translate-x-full");
       emailPopup.classList.toggle("pointer-events-none");
     } else {
-      emailPopup.classList.remove("opacity-0"); 
-      emailPopup.classList.remove("translate-x-full"); 
-      emailPopup.classList.remove("pointer-events-none"); 
-      emailPopup.classList.remove("translate-y-[-50%]"); 
+      emailPopup.classList.remove("opacity-0");
+      emailPopup.classList.remove("translate-x-full");
+      emailPopup.classList.remove("pointer-events-none");
+      emailPopup.classList.remove("translate-y-[-50%]");
     }
   });
 
   closeIcon.addEventListener("click", function () {
     if (window.innerWidth < 1024) {
-      emailPopup.classList.add("opacity-0"); 
-      emailPopup.classList.add("translate-x-full"); 
-      emailPopup.classList.add("pointer-events-none"); 
-    } else {
-      emailPopup.classList.add("opacity-0"); 
-      emailPopup.classList.add("translate-y-[-50%]"); 
+      emailPopup.classList.add("opacity-0");
+      emailPopup.classList.add("translate-x-full");
       emailPopup.classList.add("pointer-events-none");
-      emailPopup.classList.add("translate-y-0"); 
+    } else {
+      emailPopup.classList.add("opacity-0");
+      emailPopup.classList.add("translate-y-[-50%]");
+      emailPopup.classList.add("pointer-events-none");
+      emailPopup.classList.add("translate-y-0");
     }
   });
 });
@@ -89,8 +89,8 @@ document.addEventListener("DOMContentLoaded", function () {
       button.classList.toggle("rotate-180");
       faqAnswer.classList.toggle("mt-2");
 
-      faqNumber.classList.toggle("bg-secondary-600")
-      faqNumber.classList.toggle("text-white")
+      faqNumber.classList.toggle("bg-secondary-600");
+      faqNumber.classList.toggle("text-white");
 
       if (faqAnswer.classList.contains("max-h-0")) {
         faqAnswer.classList.remove("max-h-0", "opacity-0");
@@ -106,27 +106,61 @@ document.addEventListener("DOMContentLoaded", function () {
 // position fixed
 const media = window.matchMedia("screen and (min-width:1540px)");
 if (media.matches) {
- 
-   const floatingIconEl = document.querySelector(".floating-icons");
-   floatingIconEl.style.position = "fixed"; 
-   floatingIconEl.style.bottom = "0"; 
-   floatingIconEl.style.top = "unset"; 
-}else {
-   const footerEl = document.querySelector("footer"); 
-   const footerElOffsetTop = footerEl.offsetTop; 
-   const floatingIconEl = document.querySelector(".floating-icons"); 
-    window.addEventListener("scroll", function () {
-       const st = this.scrollY; 
-        
-       if (st + window.innerHeight > footerElOffsetTop) {
-           floatingIconEl.style.position = "absolute"; 
-           floatingIconEl.style.top = `${footerElOffsetTop - floatingIconEl.offsetHeight}px`;
-           floatingIconEl.style.bottom = "unset"; 
-       } else {
-           floatingIconEl.style.position = "fixed"; 
-           floatingIconEl.style.bottom = "0"; 
-           floatingIconEl.style.top = "unset"; 
-       }
-   });
-  }
+  const floatingIconEl = document.querySelector(".floating-icons");
+  floatingIconEl.style.position = "fixed";
+  floatingIconEl.style.bottom = "0";
+  floatingIconEl.style.top = "unset";
+} else {
+  const footerEl = document.querySelector("footer");
+  const footerElOffsetTop = footerEl.offsetTop;
+  const floatingIconEl = document.querySelector(".floating-icons");
+  window.addEventListener("scroll", function () {
+    const st = this.scrollY;
 
+    if (st + window.innerHeight > footerElOffsetTop) {
+      floatingIconEl.style.position = "absolute";
+      floatingIconEl.style.top = `${
+        footerElOffsetTop - floatingIconEl.offsetHeight
+      }px`;
+      floatingIconEl.style.bottom = "unset";
+    } else {
+      floatingIconEl.style.position = "fixed";
+      floatingIconEl.style.bottom = "0";
+      floatingIconEl.style.top = "unset";
+    }
+  });
+}
+
+function loadContentHomePage() {
+  loadSearchEngine("search-engine.bc", "searchbox");
+}
+async function loadSearchEngine(url, sectionload) {
+  try {
+    var xhrobj = new XMLHttpRequest();
+    xhrobj.open("GET", url);
+    xhrobj.send();
+
+    xhrobj.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        var container = document.getElementById(sectionload);
+        container.innerHTML = xhrobj.responseText;
+
+        var scripts = container.getElementsByTagName("script");
+        for (var i = 0; i < scripts.length; i++) {
+          var scriptTag = document.createElement("script");
+
+          if (scripts[i].src) {
+            scriptTag.src = scripts[i].src;
+            scriptTag.async = false;
+          } else {
+            scriptTag.text = scripts[i].textContent;
+          }
+
+          document.head
+            .appendChild(scriptTag)
+            .parentNode.removeChild(scriptTag);
+        }
+      }
+    };
+  } catch (error) {}
+}
